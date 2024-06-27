70d5576d0b3a05e73b5630b04963e9c119a30241
Anik <anikkumarb@gmail.com>
2024-06-27 12:51:54 +0530

    removed unnecessary annotation

--- app/src/main/java/com/example/passwordmanager/db/UserCredentials.kt
+++ app/src/main/java/com/example/passwordmanager/db/UserCredentials.kt
@@ -6,7 +6,7 @@ import androidx.room.PrimaryKey
 
 @Entity(tableName = "UserCred")
 data class UserCredentials(
-    @PrimaryKey @ColumnInfo(name="sitename") val sitename: String,
+    @ColumnInfo(name="sitename") val sitename: String,
     @ColumnInfo(name = "username")val username: String,
     @ColumnInfo(name = "password")val password: String
 )
